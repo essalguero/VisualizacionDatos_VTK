b249fa3de72329cc21cae9b10054876bfb1c66da
Unknown <essalguero@yahoo.es>
2018-05-03 21:56:12 +0200

    Ejercicio01 empezado

---
+++ Ejercicio01/Ejercicio01.py
@@ -0,0 +1,21 @@
+import vtk
+from vtk import *
+
+cone_mapper = vtk.vtkPolyDataMapper()
+cone_mapper.SetInputConnection(cone_mapper.GetOutputPort())
+
+cone_actor = vtk.vtkActor()
+cone_actor.SetMapper(cone_mapper)
+
+renderer = vtk.vtkRenderer()
+renderer.AddActor(cone_actor)
+
+renderer_window = vtk.vtkRenderWindow()
+renderer_window.SetWindowName("Ejercicio 01")
+renderer_window.SetSize (800, 600)
+renderer_window.AddRenderer(renderer)
+
+interactor = vtk.vtkRenderWindowInteractor()
+interactor.SetRenderWindow(renderer_window)
+interactor.Initialize()
+interactor.Start()
